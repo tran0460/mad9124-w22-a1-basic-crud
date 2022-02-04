dbd5acff36fd21e410ce758a4750dcd297aa32ca
tran0460 <tran0460@algonquinlive.com>
2022-02-04 13:18:28 -0500

    added middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,2 +1,37 @@
 let { students, courses } = require('../data/index.js')
-console.log(students)
+const validateStudentId = (req,res, next) => {
+    const id = parseInt(req.params.id)
+    const index = students.findIndex(student => student.id === id)
+    if (index < 0) {
+        res.status(404).json({
+        errors: [
+        {
+            status: '404',
+            title: 'Resource does not exist',
+            description: `We could not find a student with id: ${id}`
+        }
+        ]
+    })
+    }
+    req.studentIndex = index
+    next()
+}
+const validateCourseId = (req,res, next) => {
+    const id = parseInt(req.params.id)
+    const index = courses.findIndex(course => course.id === id)
+    if (index < 0) {
+        res.status(404).json({
+        errors: [
+        {
+            status: '404',
+            title: 'Resource does not exist',
+            description: `We could not find a course with id: ${id}`
+        }
+        ]
+    })
+    }
+    req.courseIndex = index
+    next()
+}
+
+module.exports = { validateStudentId, validateCourseId }
